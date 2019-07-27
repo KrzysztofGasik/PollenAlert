b2690c0adb4dabae48e6b1a7acdb32fff5bd2f60
KrzGas <kgasik@gmail.com>
2019-07-27 15:33:56 +0200

    zmiana w Info - zamiana klasy na hook-a

--- src/Components/MainWindow/Info/Info.js
+++ src/Components/MainWindow/Info/Info.js
@@ -1,66 +1,29 @@
-import React, { Component,Fragment } from "react";
+import React, { Component, Fragment, useState, useEffect } from "react";
 import Steps from "./Steps";
 
-class Info extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      step: 0
-    };
-  }
+export const Info = () => {
+  const [step, updateStep] = useState(0);
 
-  moveLeft = () => {
-    this.setState(
-      {
-        step: this.state.step - 1
-      },
-      () => {
-        if (this.state.step == -1) {
-          this.setState({
-            step: Steps.length-1
-          });
-        }
-      }
-    );
-  };
-
-  moveRight = () => {
-    this.setState(
-      {
-        step: this.state.step + 1
-      },
-      () => {
-        if (this.state.step == Steps.length) {
-          this.setState({
-            step: 0
-          });
-        }
-      }
-    );
-  };
-
-  render() {
-    return (
-      <>
-        <i className="fas fa-arrow-left" onClick={this.moveLeft} />
-        <div className="info__wrapper">
-          <div className="info__wrapper__slider">
-            {Steps.filter(s => s.index == this.state.step).map(info => {
-              return (
-                  <Fragment key={info.index}>
-                  <p>{info.text}</p>
-                  <p id="break">{info.break}</p>
-                  <blockquote><a href="http://www.medonet.pl/magazyny/abc-alergii,alergia---objawy--rodzaje-i-leczenie-alergii--uczulenie-na-pylki,artykul,1587489.html"
-                  target="_blank">Źródło</a></blockquote>
-                  </Fragment>
-              );    
-            })}
-          </div>
+  return (
+    <>
+      <i className="fas fa-arrow-left" onClick={() => updateStep(step > 0 ? step - 1 : Steps.length - 1)}/>
+      <div className="info__wrapper">
+        <div className="info__wrapper__slider">
+          {Steps.filter(s => s.index == step).map(info => {
+            return (
+              <Fragment key={info.index}>
+                <p>{info.text}</p>
+                <p id="break">{info.break}</p>
+                <blockquote>
+                  <a href="http://www.medonet.pl/magazyny/abc-alergii,alergia---objawy--rodzaje-i-leczenie-alergii--uczulenie-na-pylki,artykul,1587489.html"
+                    target="_blank">Źródło www.medonet.pl</a>
+                </blockquote>
+              </Fragment>
+            );
+          })}
         </div>
-        <i className="fas fa-arrow-right" onClick={this.moveRight} />
-      </>
-    );
-  }
-}
-
-export { Info };
+      </div>
+      <i className="fas fa-arrow-right" onClick={() => updateStep(step < Steps.length - 1 ? step + 1 : 0)}/>
+    </>
+  );
+};
